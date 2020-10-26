50d123ce32b88aa3691375921704591fc9823b18
Ziapa <SmiT4eggg@bk.ru>
2020-10-26 19:24:52 +0300

    Updates

---
+++ precache-manifest.dbfa1fe6e1e9f1f80e48b9ae8f9fa26a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a53828082be84206e7438d2e70b01c",
+    "url": "/no_tag/index.html"
+  },
+  {
+    "revision": "ce92c7dc48e413ba50de",
+    "url": "/no_tag/static/css/main.93e2d37c.chunk.css"
+  },
+  {
+    "revision": "5954ac32e835dd29a316",
+    "url": "/no_tag/static/js/2.07760126.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/no_tag/static/js/2.07760126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce92c7dc48e413ba50de",
+    "url": "/no_tag/static/js/main.6469e9b8.chunk.js"
+  },
+  {
+    "revision": "831595e4314da0388e9a",
+    "url": "/no_tag/static/js/runtime-main.32ee7a43.js"
+  }
+]);
